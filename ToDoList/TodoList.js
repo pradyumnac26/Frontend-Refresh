956c7828c9a7a31a0d4ff040b269de91dccbf324
Pradyumna C <69765504+pradyumnac26@users.noreply.github.com>
2025-05-17 11:14:51 -0600

    Create TodoList.js

---
+++ ToDoList/TodoList.js
@@ -0,0 +1,57 @@
+import React, {useState} from "react";
+
+
+function TodoList() {
+  const [input, setInput] = useState("");
+
+  const [todoList, setTodoList] = useState([]);
+
+
+  const deleteTodo = (id) => {
+    setTodoList(todoList.filter((t) => (t.id!==id)))
+  }
+
+ const addTodoItem = () => {
+    if (input.trim() === "")
+      return;
+    const item = {
+      id: todoList.length + 1,
+      text: input,
+      completed : false
+    }
+    setTodoList(prev => [...prev, item]);
+    setInput("")
+  }
+
+  const toggleCompleted = (id) => {
+    setTodoList(todoList.map(t => {
+      if (t.id === id) {
+        return {
+          ...t,
+          completed: !t.completed
+      }
+    }
+      else {
+        return t;
+    }
+  }))
+}
+
+
+    return (
+        <div>
+
+            <h2>Todo list</h2>
+        <input type="text" placeholder="Enter todo" value={input}
+          onChange={(e) => setInput(e.target.value)} />
+        <button onClick={()=>addTodoItem()}>Add</button>
+        <ul>
+          {todoList.map(t => <li key={t.id}><input type="checkbox" checked={t.completed} onChange={() =>toggleCompleted(t.id)} />
+            <span className = {t.completed ? "strikeThrough" : ""}>{t.text} </span>
+            <button onClick={() => deleteTodo(t.id)}>Delete</button></li>)}
+        </ul>
+        </div>
+    );
+}
+
+export default TodoList;
